7220fbe2514763613adadd350dd02979a9bce908
Artur Deters <artur.m.deters@gmail.com>
2023-05-31 21:39:14 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function aboutMe() {
+     open("./cancel.html")
+}
+function works() {
+     open("./trabalhos/index.html")
+}
---
+++ trabalhos/desafios/desafio_6/script.js
@@ -0,0 +1,5 @@
+function virus() {
+     window.alert('Não confie em anúncios que falam que não tem vírus, provavelmente tem');
+     window.alert('Não clique em anúncios que falam que você ganhou um prêmio, provavelmente é vírus');
+
+}
